9c8c4a0d764a324d9d825e3904f6e5647215bd09
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2021-04-21 22:11:56 +0530

    Add cleaner grid optimization2

--- 02-grid-traveller/03.py
+++ 02-grid-traveller/03.py
@@ -4,19 +4,16 @@ def getWays(x, y):
     # Similar to Fibonacci 03, using bottom up technique
     table = []
 
-    # Adding first base case
-    table.append([1] * y)
-
     # Creating the matrix
-    for _ in range(x-1):
+    for _ in range(x):
         table.append([0] * y)
 
-    # Adding base case, which is that the rows with 1, only have 1 way, to follow that path
     for i in range(x):
-        table[i][0] = 1
+        for j in range(y):
+            if i == 0 or j == 0:
+                table[i][j] = 1
+                continue
 
-    for i in range(1, x):
-        for j in range(1, y):
             table[i][j] = table[i-1][j] + table[i][j-1]
 
     return table[x-1][y-1]
